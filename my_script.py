2167a881c0c4082882e67fcfc6879e4e7e5f00e9
zaranabhalala <zaranabhalala1996@gmail.com>
2021-03-19 14:50:50 -0400

    Task: Updated python file

--- my_script.py
+++ my_script.py
@@ -5,3 +5,4 @@ from pystrich.datamatrix import DataMatrixEncoder
 encoder = DataMatrixEncoder('This is a DataMatrix.')
 encoder.save('./datamatrix_test.png')
 print(encoder.get_ascii())
+Print("Hello")
